03804148f31f9e498c64aa6e84cbaf3fc9ecd9b8
irackson <inasusr@gmail.com>
2024-07-08 16:55:38 -0400

    fix eslint command to include all

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,12 @@ const config = {
   parserOptions: {
     project: true,
   },
+
   plugins: ["@typescript-eslint"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
+
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   overrides: [
@@ -16,6 +18,7 @@ const config = {
       files: ["**/.github/workflows/*.yml"],
       plugins: ["yaml"],
       extends: ["plugin:yaml/legacy"],
+      rules: {},
     },
   ],
   rules: {
