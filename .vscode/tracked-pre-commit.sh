14554696774539bc7bbd988882d5da2b7a7d56a0
irackson <inasusr@gmail.com>
2024-07-09 06:32:04 -0400

    add pre-commit init code to package json

---
+++ .vscode/tracked-pre-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Add .gitkeep files to empty directories
+# find . -type d -empty -exec touch {}/.gitkeep \;
+# Add the .gitkeep files to the staging area
+# git add -A .
+
+echo "xhello from pre-commit"
---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "\nRunning Post-Install Script\n---------------------------"
+
+# create pre-commit hook in gitignored .git directory
+cat postinstall.sh
+mkdir -p .git/hooks
+cp .vscode/tracked-pre-commit.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo "Created Pre-Commit Hook"
+
+echo "\n---------------------------\nPost-Install Script Complete\n"
